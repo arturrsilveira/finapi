706b3c313b359ddc04f84d528a284d45b9697a2c
Artur <arturrsilveira85@gmail.com>
2021-10-12 19:51:35 -0300

    Rota de Cadastro de Conta Adicionada

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Funcionando...!"});
+app.use(express.json());
+
+/*
+* cpf - string
+* name - string
+* id - uuid
+* statement - []
+*/
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statemente: []
+    });
+    return response.status(201).json(customers);
 });
 
 app.listen(3333);
